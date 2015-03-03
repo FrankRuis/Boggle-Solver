b4fcd9ab73dfde1bed468410e00a270ffdbbc523
FrankRuis <github@frank-ruis.nl>
2015-03-03 14:42:31 +0100

    Robot now types instead if using copy and paste

--- Boggle Solver/src/solver/AutoFiller.java
+++ Boggle Solver/src/solver/AutoFiller.java
@@ -2,21 +2,72 @@ package solver;
 
 import java.awt.AWTException;
 import java.awt.Robot;
-import java.awt.Toolkit;
-import java.awt.datatransfer.Clipboard;
-import java.awt.datatransfer.StringSelection;
 import java.awt.event.KeyEvent;
 import java.util.List;
 
 /**
- * Automatically copy and paste the words in a selected input field and press enter
+ * Automatically type words in a selected input field and press enter
  * 
  * @author Frank
  */
 public class AutoFiller {
+	
+    private Robot robot;
 
-	public static void main(String[] args) {
-		BoardSolver solver = new BoardSolver(5, "Data/sowpods.txt");
+    /**
+     * Constructor
+     * @throws AWTException
+     */
+    public AutoFiller() throws AWTException {
+        this.robot = new Robot();
+    }
+
+    /**
+     * Constructor
+     * @param robot
+     */
+    public AutoFiller(Robot robot) {
+        this.robot = robot;
+    }
+
+    /**
+     * Type the given String and wait for the amount of milliseconds given by delay
+     * @param s
+     * @param delay
+     */
+    public void typeString(String s, int delay) {
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            
+            if (Character.isUpperCase(c)) {
+                robot.keyPress(KeyEvent.VK_SHIFT);
+            }
+            
+            robot.keyPress(Character.toUpperCase(c));
+            robot.keyRelease(Character.toUpperCase(c));
+
+            if (Character.isUpperCase(c)) {
+                robot.keyRelease(KeyEvent.VK_SHIFT);
+            }
+        }
+        
+        robot.delay(delay);
+    }
+    
+    /**
+     * Press the enter key and wait for the amount of milliseconds given by delay
+     * @param delay
+     */
+    public void enter(int delay) {
+    	robot.keyPress(KeyEvent.VK_ENTER);
+    	robot.keyRelease(KeyEvent.VK_ENTER);
+    	robot.delay(delay);
+    }
+
+	public static void main(String[] args) throws AWTException {
+		AutoFiller filler = new AutoFiller();
+
+		BoardSolver solver = new BoardSolver(5, "Data/TWL06.txt");
 		solver.sortWordList(false);
 		List<Path> paths = solver.getWordList();
 		
@@ -32,26 +83,8 @@ public class AutoFiller {
 		// Copy and paste each word and press enter
 		for (Path s : paths) {
 			if (s.size() >= minLength) {
-				try {
-					Robot robot = new Robot();
-					Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
-					
-					StringSelection stringSelection = new StringSelection(s.toString());
-					clipboard.setContents(stringSelection, stringSelection);
-		
-					robot.keyPress(KeyEvent.VK_CONTROL);
-					robot.keyPress(KeyEvent.VK_V);
-					robot.keyRelease(KeyEvent.VK_V);
-					robot.keyRelease(KeyEvent.VK_CONTROL);
-					robot.keyPress(KeyEvent.VK_ENTER);
-					robot.keyRelease(KeyEvent.VK_ENTER);
-					
-					Thread.sleep(150);
-				} catch (AWTException e) {
-					e.printStackTrace();
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
+				filler.typeString(s.toString(), 0);
+				filler.enter(10);
 			}
 		}
 	}
